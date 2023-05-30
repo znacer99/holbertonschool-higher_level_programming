3e944fbff33cc42d8557607337dc25c2ef849b1a
root <6274@holbertonstudents.com>
2023-05-30 14:51:57 -0700

    3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for c in range(97, 123):
-    if chr(c) not in ['q' , 'e']:
-        print("{}".format(chr(c)), end="")
+    if chr(c) != ('q') and chr(c) != ('e'):
+        print('{}'.format(chr(c)), end="")
