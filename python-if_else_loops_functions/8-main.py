61f2ecfee382f755b0cf7193ab9e25856642e4e9
root <6274@holbertonstudents.com>
2023-05-31 16:46:12 -0700

    main function

--- python-if_else_loops_functions/8-main.py
+++ python-if_else_loops_functions/8-main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 uppercase = __import__('8-uppercase').uppercase
-
-uppercase('best')
-uppercase('best school 98 battery street')
+uppercase('best\n')
+uppercase('best school 98 battery street\n')
