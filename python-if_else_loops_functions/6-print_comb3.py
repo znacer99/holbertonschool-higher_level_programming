730afd2f83c5289c8cb94ed9ebeee4a007b37286
root <6274@holbertonstudents.com>
2023-05-31 09:46:17 -0700

    6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
+for i in range(0, 10):
+    for j in range((i+1), 10):
         if (i is not 8) or (j is not 9):
             print("{}{}, ".format(i,j), end="")
         else:
