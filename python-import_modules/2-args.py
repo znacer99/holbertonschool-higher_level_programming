3ea2bb8951b6116f77925925518eacb2e6bfbcfb
root <6274@holbertonstudents.com>
2023-05-31 19:48:56 -0700

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 import sys
-
 if __name__ == "__main__":
     argv = sys.argv[1:]
     num_args = len(argv)
-
     if num_args == 0:
         print("0 arguments.")
-
     else:
         print(f"{num_args} {'argument' if num_args == 1 else 'arguments'}:")
         for i, arg in enumerate(argv, start=1):
