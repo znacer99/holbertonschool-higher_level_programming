40be2d59e092a871772b125dbf403571a139e389
root <6274@holbertonstudents.com>
2023-05-31 17:13:24 -0700

    9

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,4 @@ def print_last_digit(number):
         number = number * -1
     last_digit = number % 10
     print("{:d}".format(last_digit), end="")
-    return last_digit
-
+    return(last_digit)
