68a03e9478646dcf1225bf926bf8e0aeb816c679
root <6274@holbertonstudents.com>
2023-06-25 12:12:06 -0700

    3

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for element in set_1:
-        for element in set_2:
-            if element in set_1 and element in set_2:
-                return element
+    common_set = set_1.intersection(set_2)
+    return common_set
