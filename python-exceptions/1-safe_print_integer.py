3e759781c6ea047b2c1fc1547f533e566676ba32
root <6274@holbertonstudents.com>
2023-06-26 09:32:42 -0700

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,5 @@ def safe_print_integer(value):
     try:
         print ("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
-        pass
+    except:
         return False
