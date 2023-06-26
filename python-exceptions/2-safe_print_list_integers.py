42c177bc1471f23d31a143dd8c5acae778338d50
root <6274@holbertonstudents.com>
2023-06-26 07:57:16 -0700

    2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,7 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
                 if count == x:
                     break
-    except IndexError:
-        pass
-    print()
-    return count
+        print()
+        return count
+    except:
+       pass
