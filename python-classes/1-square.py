98c33a322c5365591b1e8ed1c04518783cb3cd54
root <6274@holbertonstudents.com>
2023-06-27 13:44:08 -0700

    squary

---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+    """Write a class Square that defines a square"""
+    def __init__(self, size_square):
+        """Module __init__ for size square"""
+        self.__size = size_square
