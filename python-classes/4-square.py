1cae09746857e1f2650c99bf9f5bb31916e5752b
root <6274@holbertonstudents.com>
2023-06-29 11:52:30 -0700

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,17 +17,12 @@ class Square:
 
     @property
     def size(self):
-
-        """setter for Square"""
-
         return self.__size
 
 
 
     @size.setter
     def size(self, value):
-        """Initializes size setter"""
-
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -38,7 +33,4 @@ class Square:
 
 
     def area(self):
-
-        """Calculate area of Square"""
-
         return self.__size ** 2
