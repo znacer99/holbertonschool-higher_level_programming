ea06e1ef5e4ddd607920aff24e1522244d5cf420
root <6274@holbertonstudents.com>
2023-06-29 11:28:21 -0700

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Check type"""
+
+
+class Square:
+    """Define methods of Square"""
+
+    def __init__(self, size=0):
+        """Initializes attribute of size"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """calculate area of Square"""
+        return self.__size ** 2
