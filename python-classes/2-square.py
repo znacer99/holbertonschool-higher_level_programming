f93fc2f6f169ed37d8b57548e74a1b91c4a7e052
root <6274@holbertonstudents.com>
2023-06-29 11:12:26 -0700

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with optional """
+
+    def __init__(self, size=0):
+        """Initializes attribute size """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
