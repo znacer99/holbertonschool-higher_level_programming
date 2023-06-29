8ac031765b4190d07217405d904b3c26e7a8f598
root <6274@holbertonstudents.com>
2023-06-29 13:38:44 -0700

    6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,7 +34,7 @@ class Square:
         """Setter method for the position attribute"""
         if not isinstance(value, tuple) or len(value) != 2 or \
                 not all(isinstance(num, int) for num in value) or \
-                not all(num >= 0 for num in value):
+                not all(num < 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
