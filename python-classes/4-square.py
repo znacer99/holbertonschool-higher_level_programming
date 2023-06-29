f5932666d3dedf29f299429b2c0ebb2dfb589283
root <6274@holbertonstudents.com>
2023-06-29 11:49:35 -0700

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -8,25 +8,37 @@ class Square:
     """Define methods of Square"""
 
     def __init__(self, size=0):
+
         """Initializes attribute size"""
+
         self.__size = size
+
+
 
     @property
     def size(self):
+
         """setter for Square"""
+
         return self.__size
+
+
 
     @size.setter
     def size(self, value):
         """Initializes attribute size"""
+
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >=")
         else:
-            self.__size
+            self.__size = value
+
+
 
     def area(self):
+
         """Calculate area of Square"""
 
         return self.__size ** 2
