e43865f87f42f7505a89c13a902954e47c1758b8
root <6274@holbertonstudents.com>
2023-06-30 15:22:21 -0700

    3

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -53,7 +53,8 @@ class Rectangle:
 
         if self.__width == 0 or self.__height == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        else:
+            return 2 * (self.__width + self.__height)
 
     def __str__(self):
         """
