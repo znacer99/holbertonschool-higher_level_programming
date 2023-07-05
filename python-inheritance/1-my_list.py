cf09d9d8d674b945185803fa4d52ce700d1524dd
root <6274@holbertonstudents.com>
2023-07-05 11:20:01 -0700

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""Define Mylist that inherits from List"""
+"""define class Mylist that inherite from class list"""
 
 
 class MyList(list):
-    """Define methods of class"""
+    """define fields and metod of my class"""
 
     def print_sorted(self):
-        """Print sorted list"""
-
-        sorted_list = sorted(self)
-        print(sorted_list)
+        """print sorted list"""
+        print (sorted(self))
