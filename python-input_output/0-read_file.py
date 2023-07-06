2b6b79e0e378cd02a230879eed969a224232320c
root <6274@holbertonstudents.com>
2023-07-05 20:42:35 -0700

    0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""Define function"""
 
-def read_file(filename="my_file_0.txt"):
+
+def read_file(filename=""):
+    """Read and print data"""
+
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
         print(content)
